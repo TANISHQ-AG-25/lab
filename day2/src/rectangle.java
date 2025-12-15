df3f7b775a6b4e0bb4aa19925e9f7f75955517a8
tanishq <24051221@kiit.ac.in>
2025-12-15 23:42:37 +0530

    day2

--- day2/src/rectangle.java
+++ day2/src/rectangle.java
@@ -1,15 +1,24 @@
+
 import java.util.Scanner;
 public class rectangle {
     int length;
     int breadth;
-    rectangle(int length,int breadth)
+    int area;
+    int perimeter;
+    void read(int length, int breadth)
     {
         this.length=length;
         this.breadth=breadth;
     }
-    void read()
+    void calculate()
     {
-
+        this.area = this.length * this.breadth;
+        this.perimeter = 2*(this.length + this.breadth);
+    }
+    void display()
+    {
+        System.out.println("Area = " + this.area);
+        System.out.println("Perimeter = " + this.perimeter);
     }
 }
 class data
@@ -17,8 +26,13 @@ class data
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length");
         int length = sc.nextInt();
+        System.out.println("Enter breadth");
         int breadth = sc.nextInt();
-        rectangle rec = new rectangle(length,breadth);
+        rectangle rec = new rectangle();
+        rec.read(length,breadth);
+        rec.calculate();
+        rec.display();
     }
 }
