124395f74bce7f27ae275ed926a283ef0739588d
tanishq <24051221@kiit.ac.in>
2025-12-15 16:54:26 +0530

    day2

--- day2/src/objcount.java
+++ day2/src/objcount.java
@@ -1,2 +1,21 @@
 public class objcount {
+    static int c=0;
+    objcount()
+    {
+        this.c++;
+    }
+    void display()
+    {
+        System.out.println("total no of objects is " + this.c);
+    }
+}
+class obj{
+    static void main() {
+        objcount o1 = new objcount();
+        objcount o2 = new objcount();
+        objcount o3 = new objcount();
+        objcount o4 = new objcount();
+        objcount o5 = new objcount();
+        o1.display();
+    }
 }
